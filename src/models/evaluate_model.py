2090d3c46c3b6b318ad6e403e3cd1fab0845a5c4
Melanie94480 <bessonnier.melanie@gmail.com>
2026-07-22 18:04:47 +0200

    simplify CI lint rules and fix unused imports

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import json
 import logging
-import mlflow
 from sklearn.metrics import (
     accuracy_score,
     precision_score,
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,11 +1,13 @@
 
-import sklearn
+
 import pandas as pd 
 from sklearn import ensemble
 import joblib
 import numpy as np
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import dagshub
+import mlflow
+import mlflow.sklearn
 #dagshub.init(repo_owner='Melanie94480', repo_name='mlops-melanie', mlflow=True)
 
 
